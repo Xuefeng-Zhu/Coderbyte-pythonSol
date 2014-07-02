79ea204df3844f3ba0b0b2801be935ad6a2d7429
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-01 23:27:02 -0400

    p2

---
+++ 2-factorial.py
@@ -0,0 +1,8 @@
+import math
+def FirstFactorial(num): 
+    return math.factorial(num)
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstFactorial(raw_input())           
