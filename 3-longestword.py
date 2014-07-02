7f37eb4d726d2146b987db5366bdb2e53644f978
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-02 00:04:48 -0400

    p3

---
+++ 3-longestword.py
@@ -0,0 +1,17 @@
+# def LongestWord(sen): 
+#   return max((len(word),word) for word in sen.split() if word.isalpha())[1]
+    
+    
+# # keep this function call here  
+# # to see how to enter arguments in Python scroll down
+# print LongestWord(raw_input())           
+def LongestWord(sen): 
+   max = (0,"")
+   for i in [(len(word),word) for word in sen.split() if word.isalpha()]:
+     if (i[0] > max[0]):
+        max = i
+   return max[1]
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print LongestWord(raw_input())           
