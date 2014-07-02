4a6d69bec2ad45df87a892ec02f4fd6c09a52caf
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-01 23:17:49 -0400

    problem1

---
+++ 1-reverse.py
@@ -0,0 +1,10 @@
+def FirstReverse(str): 
+
+  # code goes here
+
+  return str[::-1]
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstReverse(raw_input())           
