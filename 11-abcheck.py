169d28f9615ce259cddf374b2bb188d269e5bd5b
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 02:52:51 -0400

    p12

--- 11-abcheck.py
+++ 11-abcheck.py
@@ -1,3 +1,6 @@
+# Have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false. 
+
+# Use the Parameter Testing feature in the box below to test your code with different arguments.
 def ABCheck(str): 
   return "true" if any(i.find("b")==3 for i in str.split("a")) else "false"
     
---
+++ 12-vowelCount.py
@@ -0,0 +1,12 @@
+def VowelCount(str): 
+    return len([i for i in str if i in "aeuio"])
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print VowelCount(raw_input())  
+
+
+
+
+
+
