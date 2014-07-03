ece3115a28835668118e04d71ab19f10b37c0752
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 03:10:08 -0400

    p15

---
+++ 15-palindrome.py
@@ -0,0 +1,10 @@
+def Palindrome(str): 
+  str = str.replace(" ", "")
+  return "true" if str == str[::-1] else "false"
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print Palindrome("dont nod")  
+
+
