f7dd4ba3a5e5a4725c47a566f01144ae1ebac493
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 02:28:29 -0400

    p10

---
+++ 10-alphabet-soup.py
@@ -0,0 +1,13 @@
+def AlphabetSoup(str): 
+  return "".join(sorted(str))
+
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print AlphabetSoup(raw_input())  
+
+
+
+
+
+
