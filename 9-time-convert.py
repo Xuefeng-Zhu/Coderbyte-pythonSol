3bfcb354888db40d8fb3f7bfcdfba7319567c283
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 02:22:38 -0400

    p9

---
+++ 9-time-convert.py
@@ -0,0 +1,7 @@
+def TimeConvert(num): 
+   return str(num/60) + ":" + str(num%60)
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print TimeConvert(raw_input())  
