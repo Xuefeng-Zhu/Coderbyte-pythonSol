f4ccc028a56946811622f574216a8b2a01542609
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 01:21:48 -0400

    p6

---
+++ 6-letter-capitalize.py
@@ -0,0 +1,16 @@
+def LetterCapitalize(str): 
+  return " ".join(i.capitalize() for i in str.split())
+ 
+       
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print LetterCapitalize(raw_input())  
+
+
+
+
+
+
+
+
+
