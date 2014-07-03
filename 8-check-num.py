cc77baa7a908474532637094885d277283e14b0e
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 02:22:05 -0400

    p8

---
+++ 8-check-num.py
@@ -0,0 +1,15 @@
+def CheckNums(num1,num2): 
+  if num1 == num2:
+    return "-1"
+  return "true" if num1 < num2 else "false"
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print CheckNums(raw_input())  
+
+
+
+
+
+
