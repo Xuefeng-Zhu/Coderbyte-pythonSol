b7f27abc185f36c8f54a03a0f82147180db5320c
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-03 02:58:57 -0400

    p13

---
+++ 13-count-word.py
@@ -0,0 +1,11 @@
+def WordCount(str): 
+  return len(str.split())
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print WordCount(raw_input())  
+
+
+
+
