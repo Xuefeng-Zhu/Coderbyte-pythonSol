d3c34cb1616f5053ad5ac8d6eafbe7fab1afa62b
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-04 22:38:02 -0400

    p16

---
+++ 16-Arith-Geo.py
@@ -0,0 +1,16 @@
+def ArithGeo(arr): 
+  result = "Arithmetic"
+  for i in range(1,len(arr)-1):
+    if arr[i+1] - arr[i] != arr[1] - arr[0]:
+      result = "Geometric"
+      for i in range(1, len(arr)-1):
+        if arr[i+1] / arr[i] != arr[1] / arr[0]:
+          result = -1
+      break
+  return result
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print ArithGeo(raw_input())  
+
+
