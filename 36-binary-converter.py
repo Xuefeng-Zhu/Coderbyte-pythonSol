a95ff9cd0bb6560a1b299eb7d3b6c448ae6cd7a9
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-06 17:03:02 -0400

    p36

---
+++ 36-binary-converter.py
@@ -0,0 +1,23 @@
+def BinaryConverter(str): 
+  return int(str, 2)
+  
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print BinaryConverter(raw_input())  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
