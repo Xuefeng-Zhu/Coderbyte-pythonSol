2c4fe5414efba0d58156c34eae5129488551a440
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-06 18:37:39 -0400

    p43

--- 42-permutation.py
+++ 42-permutation.py
@@ -1,20 +1,36 @@
-def swap(nList,i):
-  nList[i],nList[i-1] = nList[i-1], nList[i]
-  for x in range(i, -1):
-    if int(nList[i]) > int(nList[i+1]):
-      nList[i],nList[i+1] = nList[i+1], nList[i]
 
-def PermutationStep(num): 
-  nList = list(str(num))
-  for i in range(-1, -len(nList), -1):
-    if int(nList[i]) > int(nList[i-1]):
-      swap(nList,i)
-      return "".join(nList)
-  return -1
+# def swap(nList,i):
+#   nList[i],nList[i-1] = nList[i-1], nList[i]
+#   for x in range(i, -1):
+#     if int(nList[i]) > int(nList[i+1]):
+#       nList[i],nList[i+1] = nList[i+1], nList[i]
+
+# def PermutationStep(num): 
+#   nList = list(str(num))
+#   for i in range(-1, -len(nList), -1):
+#     if int(nList[i]) > int(nList[i-1]):
+#       swap(nList,i)
+#       return "".join(nList)
+#   return -1
     
+# # keep this function call here  
+# # to see how to enter arguments in Python scroll down
+# print PermutationStep(raw_input())  
+
+# Using the Python language, have the function PermutationStep(num) take the num parameter being passed and return the next number greater than num using the same digits. For example: if num is 123 return 132, if it's 12453 return 12534. If a number has no greater permutations, return -1 (ie. 999). 
+
+# Use the Parameter Testing feature in the box below to test your code with different arguments.
+def PermutationStep(num): 
+  temp = str(num)
+  for i in range(len(temp)-1, 0, -1):
+    if temp[i] > temp[i-1]:
+      return temp[:i-1] + temp[i] + "".join(sorted(temp[i-1]+temp[i+1:]))
+  return -1 
+        
 # keep this function call here  
 # to see how to enter arguments in Python scroll down
-print PermutationStep(raw_input())  
+print PermutationStep(raw_input())           
+
 
 
 
@@ -32,3 +48,19 @@ print PermutationStep(raw_input())
 
 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   
---
+++ 43-prime-check.py
@@ -0,0 +1,22 @@
+# Using the Python language, have the function PrimeChecker(num) take num and return 1 if any arrangement of num comes out to be a prime number, otherwise return 0. For example: if num is 910, the output should be 1 because 910 can be arranged into 109 or 019, both of which are primes. 
+
+# Use the Parameter Testing feature in the box below to test your code with different arguments.
+def is_Prime(n):
+    if n % 2 == 0:
+    	return False
+	for i in xrange(3, int(n ** 0.5)+1, 2):
+		if n % i == 0:
+			return False
+	return True
+  
+import itertools
+def PrimeChecker(num): 
+  temp = str(num)
+  for num in itertools.permutations(temp, len(temp)):
+    if is_Prime(int("".join(num))):
+      return 1
+  return 0  
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print PrimeChecker(raw_input())           
