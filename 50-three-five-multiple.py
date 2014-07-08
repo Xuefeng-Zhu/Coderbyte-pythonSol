288c5b185468791a87acdb156ba599b886dfa735
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-08 00:01:20 -0400

    p50

---
+++ 50-three-five-multiple.py
@@ -0,0 +1,23 @@
+def ThreeFiveMultiples(num): 
+  return sum(i for i in range(num) if i % 3 == 0 or i % 5 == 0)
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print ThreeFiveMultiples(raw_input())  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
