127a95956efa17e9f5f0ae8272b95c0fb07cf1c7
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-07-07 23:18:05 -0400

    p46

--- 46-number-search.py
+++ 46-number-search.py
@@ -1,25 +1,7 @@
+import math
 def NumberSearch(str): 
-  return sum(int(i) for i in str if i.isdigit())/len([i for i in str if i.isalpha()])
+  return int(round(float(sum(int(i) for i in str if i.isdigit()))/len([i for i in str if i.isalpha()])))
   
-    
-    
 # keep this function call here  
 # to see how to enter arguments in Python scroll down
-print NumberSearch(raw_input())  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+print NumberSearch(raw_input())           
